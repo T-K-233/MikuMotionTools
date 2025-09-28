282bbd3d04bfad6c08df116ee998288276b25560
-T.K.- <t_k_233@outlook.com>
2025-09-28 02:21:06 -0700

    ADD: option to view transformed frames

--- scripts/compare_frames.py
+++ scripts/compare_frames.py
@@ -15,14 +15,17 @@ import mujoco
 import mujoco.viewer
 import numpy as np
 
+from mikumotion.math import quat_mul
 from mikumotion.mujoco_utils import create_empty_scene, add_body_frames
 from mikumotion.motion_sequence import MotionSequence
+from mikumotion.presets import MMD_YYB_TO_G1_CFG
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--source", type=str, required=True, help="Source motion file")
     parser.add_argument("--target", type=str, required=True, help="Target motion file")
+    parser.add_argument("--mapping", type=str, help="Mapping config")
     args = parser.parse_args()
 
     xml = create_empty_scene(show_world_frame=True)
@@ -38,11 +41,26 @@ if __name__ == "__main__":
     model = mujoco.MjModel.from_xml_string(xml)
     data = mujoco.MjData(model)
 
+    if args.mapping:
+        mapping = MMD_YYB_TO_G1_CFG
+    else:
+        mapping = None
+
     # move the source frame mocap body
     for body_name in source_armature.body_names:
         position = source_armature.body_positions[0, source_armature.get_body_indices([body_name])[0]]
         orientation = source_armature.body_rotations[0, source_armature.get_body_indices([body_name])[0]]
         mocap_id = model.body(f"source_{body_name}_frame").mocapid[0]
+        if mapping:
+            for entry in mapping.values():
+                if entry["source"] == body_name:
+                    break
+            if entry.get("offset") and entry["offset"].get("position"):
+                position += np.array(entry["offset"]["position"])
+
+            if entry.get("offset") and entry["offset"].get("orientation"):
+                orientation = quat_mul(orientation, np.array(entry["offset"]["orientation"]))
+
         data.mocap_pos[mocap_id] = position
         data.mocap_quat[mocap_id] = orientation
 
